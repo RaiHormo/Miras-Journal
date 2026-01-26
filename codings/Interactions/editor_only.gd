ed75289298caa2698d713e897b31cfcb61dc749a
Iris <iris@raidev.eu>
2026-01-26 13:40:13 +0200

    decorated town hall

--- codings/Interactions/editor_only.gd
+++ codings/Interactions/editor_only.gd
@@ -1,6 +1,8 @@
-extends Node
+extends Node2D
 
+@export var hide_instead:= false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	queue_free()
+	if hide_instead: hide()
+	else: queue_free()
